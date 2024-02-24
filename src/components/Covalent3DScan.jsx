ffc421e0cb17432efe067c0874f0d0fa3a823e93
MATIAS TORRES <matiiiastorres@gmail.com>
2024-02-24 18:19:08 -0300

    Update Covalent3DScan.jsx
    cambie el codego del alert

--- src/components/Covalent3DScan.jsx
+++ src/components/Covalent3DScan.jsx
@@ -7,6 +7,7 @@ const CovalentTokenViewer = () => {
   const [address, setAddress] = useState('');
   const [tokensData, setTokensData] = useState([]);
   const [selectedTokenIndex, setSelectedTokenIndex] = useState(-1);
+  const [clicked, setClicked] = useState(false);
 
   const canvasRef = useRef(null);
 
@@ -101,10 +102,12 @@ const CovalentTokenViewer = () => {
   };
 
   const handleSphereClick = (token) => {
-    // Aquí puedes realizar la acción que desees cuando se hace clic en una esfera
-    alert(
-      `Información del Token:\nNombre: ${token.contract_name}\nValor: ${token.quote}`
-    );
+    if (!clicked) {
+      setClicked(true);
+      alert(
+        `Información del Token:\nNombre: ${token.contract_name}\nValor: ${token.quote}`
+      );
+    }
   };
 
   return (
@@ -161,7 +164,7 @@ const CovalentTokenViewer = () => {
 };
 
 export default CovalentTokenViewer;
-
+// export default CovalentTokenViewer;
 // import { useEffect, useRef, useState } from 'react';
 // import p5 from 'p5';
 // import './covalent3DScan.css';
@@ -177,14 +180,20 @@ export default CovalentTokenViewer;
 //   useEffect(() => {
 //     const sketch = (p) => {
 //       p.setup = () => {
-//         p.createCanvas(1100, 600, p.WEBGL).parent('tokenText');
+//         const canvasWidth =
+//           window.innerWidth > 1100 ? 1100 : window.innerWidth - 20;
+//         const canvasHeight =
+//           window.innerWidth > 1100
+//             ? 600
+//             : (window.innerWidth - 20) * (600 / 1100); // Manteniendo la proporción
+
+//         p.createCanvas(canvasWidth, canvasHeight, p.WEBGL).parent('tokenText');
 //       };
 
 //       p.draw = () => {
 //         p.background(255, 0); // Fondo transparente
-
 //         p.orbitControl();
-//         // Dibujar esferas para representar los tokens
+
 //         for (let i = 0; i < Math.min(tokensData.length, 5); i++) {
 //           const token = tokensData[i];
 //           const valueInUSD = token.quote;
@@ -200,6 +209,18 @@ export default CovalentTokenViewer;
 //           p.rotateY(rotationAngle);
 //           p.sphere(radius);
 //           p.pop();
+
+//           // Verificar clic en la esfera actual
+//           const mousePos = p.createVector(
+//             p.mouseX - p.width / 2,
+//             p.mouseY - p.height / 2
+//           );
+//           const spherePos = p.createVector(xPos, yPos, zPos);
+//           const distance = mousePos.dist(spherePos);
+//           if (distance < radius && p.mouseIsPressed) {
+//             // Verificar si se ha hecho clic
+//             handleSphereClick(token); // Llamar a la función para manejar el clic en la esfera
+//           }
 //         }
 //       };
 //     };
@@ -231,7 +252,9 @@ export default CovalentTokenViewer;
 //   const getData = async () => {
 //     try {
 //       const response = await fetch(
-//         `https://api.covalenthq.com/v1/1/address/${address}/balances_v2/?key=${import.meta.env.VITE_COVALENT_API_KEY}`
+//         `https://api.covalenthq.com/v1/1/address/${address}/balances_v2/?key=${
+//           import.meta.env.VITE_COVALENT_API_KEY
+//         }`
 //       );
 //       const json = await response.json();
 //       setTokensData(json.data.items);
@@ -244,8 +267,11 @@ export default CovalentTokenViewer;
 //     getData();
 //   };
 
-//   const handleTokenClick = (index) => {
-//     setSelectedTokenIndex(index);
+//   const handleSphereClick = (token) => {
+//     // Aquí puedes realizar la acción que desees cuando se hace clic en una esfera
+//     alert(
+//       `Información del Token:\nNombre: ${token.contract_name}\nValor: ${token.quote}`
+//     );
 //   };
 
 //   return (
@@ -267,18 +293,33 @@ export default CovalentTokenViewer;
 //           <button onClick={handleGetTokens}>Get Tokens and View in 3D</button>
 //         </div>
 //       </div>
-//       <div id="tokenText" className="token-info-container" ref={canvasRef}></div>
+//       <div
+//         id="tokenText"
+//         className="token-info-container"
+//         ref={canvasRef}
+//       ></div>
 //       <div className="token-container">
 //         {tokensData.map((token, index) => (
 //           <div
 //             key={index}
-//             className={`token ${selectedTokenIndex === index ? 'selected' : ''}`}
-//             onClick={() => handleTokenClick(index)}
-//             style={{ backgroundColor: `rgb(${getColorByToken(token.contract_name).join(',')})` }}
+//             className={`token ${
+//               selectedTokenIndex === index ? 'selected' : ''
+//             }`}
+//             onClick={() => setSelectedTokenIndex(index)}
+//             style={{
+//               backgroundColor: `rgb(${getColorByToken(token.contract_name).join(
+//                 ','
+//               )})`,
+//             }}
 //           >
 //             <h2>{token.contract_name}</h2>
 //             <h2>{token.quote}</h2>
-//            <img src={token.logo_url} alt="Token Logo" className="token-logo" style={{ width: '64px', height: '64px' }} />
+//             <img
+//               src={token.logo_url}
+//               alt="Token Logo"
+//               className="token-logo"
+//               style={{ width: '64px', height: '64px' }}
+//             />
 //           </div>
 //         ))}
 //       </div>
