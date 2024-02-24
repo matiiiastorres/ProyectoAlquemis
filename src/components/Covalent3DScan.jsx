f6c9f278873729932cdb1a0a789c1c54756056af
MATIAS TORRES <matiiiastorres@gmail.com>
2024-02-24 04:13:56 -0300

    matias mandando todo

--- src/components/Covalent3DScan.jsx
+++ src/components/Covalent3DScan.jsx
@@ -1,16 +1,75 @@
-import { useState } from 'react';
+import { useEffect, useRef, useState } from 'react';
+import p5 from 'p5';
 import './covalent3DScan.css';
-import p5 from 'p5'; // Importa p5 como una función
+import MATIVIDEO from '../components/2_2.mp4';
 
 const CovalentTokenViewer = () => {
   const [address, setAddress] = useState('');
   const [tokensData, setTokensData] = useState([]);
   const [selectedTokenIndex, setSelectedTokenIndex] = useState(-1);
 
+  const canvasRef = useRef(null);
+
+  useEffect(() => {
+    const sketch = (p) => {
+      p.setup = () => {
+        p.createCanvas(800, 400, p.WEBGL).parent('tokenText');
+      };
+
+      p.draw = () => {
+        p.background(255, 0); // Fondo transparente
+
+        p.orbitControl();
+        // Dibujar esferas para representar los tokens
+        for (let i = 0; i < Math.min(tokensData.length, 5); i++) {
+          const token = tokensData[i];
+          const valueInUSD = token.quote;
+          const radius = p.map(valueInUSD, 0, 100, 50, 200);
+          const xPos = -200 + i * 200;
+          const yPos = 0;
+          const zPos = 0;
+          const rotationAngle = p.frameCount * 0.01;
+          const sphereColor = getColorByToken(token.contract_name, p);
+          p.fill(sphereColor);
+          p.push();
+          p.translate(xPos, yPos, zPos);
+          p.rotateY(rotationAngle);
+          p.sphere(radius);
+          p.pop();
+        }
+      };
+
+      const getColorByToken = (tokenName, p) => {
+        switch (tokenName) {
+          case 'Ether':
+            return p.color(255, 0, 255);
+          case 'Tether USD':
+            return p.color(0, 255, 0);
+          case 'USD Coin':
+            return p.color(0, 0, 255);
+          case 'Wrapped Ether':
+            return p.color(255, 255, 0);
+          case 'Matic Token':
+            return p.color(0, 255, 85);
+          default:
+            return p.color(p.random(255), p.random(255), p.random(255));
+        }
+      };
+    };
+
+    const p5Instance = new p5(sketch, canvasRef.current);
+
+    return () => {
+      p5Instance.remove();
+    };
+  }, [tokensData]);
+
   const getData = async () => {
     try {
       const response = await fetch(
-        `https://api.covalenthq.com/v1/1/address/${address}/balances_v2/?key=cqt_rQxtM4C4RhdgHfXtvVCXhRmJbm4c`
+        `https://api.covalenthq.com/v1/1/address/${address}/balances_v2/?key=${
+          import.meta.env.VITE_COVALENT_API_KEY
+        }`
       );
       const json = await response.json();
       setTokensData(json.data.items);
@@ -27,96 +86,30 @@ const CovalentTokenViewer = () => {
     setSelectedTokenIndex(index);
   };
 
-  const sketch = (p) => {
-    let tokensDataCopy = [];
-
-    p.setup = () => {
-      p.createCanvas(800, 400, p.WEBGL);
-    };
-
-    p.draw = () => {
-      p.background(0);
-
-      p.orbitControl();
-
-      for (let i = 0; i < Math.min(tokensDataCopy.length, 5); i++) {
-        let token = tokensDataCopy[i];
-        let valueInUSD = token.quote;
-        let radius = p.map(valueInUSD, 0, 100, 50, 200);
-        let xPos = -200 + i * 200;
-        let yPos = 0;
-        let zPos = 0;
-        let rotationAngle = p.frameCount * 0.01;
-
-        let sphereColor = p.color(p.random(255), p.random(255), p.random(255));
-
-        if (token.contract_name === 'Ether') {
-          sphereColor = p.color(255, 0, 255);
-        } else if (token.contract_name === 'Tether USD') {
-          sphereColor = p.color(0, 255, 0);
-        } else if (token.contract_name === 'USD Coin') {
-          sphereColor = p.color(0, 0, 255);
-        } else if (token.contract_name === 'Wrapped Ether') {
-          sphereColor = p.color(255, 255, 0);
-        } else if (token.contract_name === 'Matic Token') {
-          sphereColor = p.color(0, 255, 85);
-        }
-
-        p.fill(sphereColor);
-        p.push();
-        p.translate(xPos, yPos, zPos);
-        p.rotateY(rotationAngle);
-        p.sphere(radius);
-        p.pop();
-
-        let distance = p.dist(
-          p.mouseX - p.width / 2,
-          p.mouseY - p.height / 2,
-          xPos,
-          yPos
-        );
-        if (distance < radius) {
-          setSelectedTokenIndex(i);
-        }
-      }
-    };
-
-    p.mouseClicked = () => {
-      if (selectedTokenIndex !== -1) {
-        let selectedToken = tokensDataCopy[selectedTokenIndex];
-        alert(
-          `Token Information:\nName: ${selectedToken.contract_name}\nValue: ${selectedToken.quote}`
-        );
-      }
-    };
-
-    p.myCustomRedrawAccordingToNewPropsHandler = (newTokensData) => {
-      tokensDataCopy = newTokensData.slice();
-    };
-  };
-
-  let myp5;
-
-  const ref = (node) => {
-    if (node != null) {
-      myp5 = new p5(sketch, node);
-    }
-  };
-
   return (
-    <div className="covalent-token-viewer">
-      <h1 className="title">Covalent3DScan</h1>
-      <div className="input-container">
-        <label htmlFor="addressInput"> Address:</label>
-        <input
-          type="text"
-          id="addressInput"
-          value={address}
-          onChange={(e) => setAddress(e.target.value)}
-          placeholder="Ethereum Address"
-        />
-        <button onClick={handleGetTokens}>Get Tokens and View in 3D</button>
+    <>
+      <video autoPlay loop muted className="video-background">
+        <source src={MATIVIDEO} type="video/mp4" />
+      </video>
+      <div className="covalent-token-viewer">
+        <h1 className="title">Covalent3DScan</h1>
+        <div className="input-container">
+          <label htmlFor="addressInput"> Address:</label>
+          <input
+            type="text"
+            id="addressInput"
+            value={address}
+            onChange={(e) => setAddress(e.target.value)}
+            placeholder="Ethereum Address"
+          />
+          <button onClick={handleGetTokens}>Get Tokens and View in 3D</button>
+        </div>
       </div>
+      <div
+        id="tokenText"
+        className="token-info-container"
+        ref={canvasRef}
+      ></div>
       <div className="token-container">
         {tokensData.map((token, index) => (
           <div
@@ -131,9 +124,7 @@ const CovalentTokenViewer = () => {
           </div>
         ))}
       </div>
-      <div id="tokenText" className="token-info-container"></div>
-      <div ref={ref}></div>
-    </div>
+    </>
   );
 };
 
